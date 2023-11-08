a73d5ac62273b4ea5e3f763b6b63e2a77fe8aa1b
Dhiraj Mani Tiwari <60131419+Manidhiraj@users.noreply.github.com>
2023-11-08 11:15:25 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version standerd 
+// Java Script Engline is inside the browser 
+
+
+
+alert(3 + 3) // We are using nodejs, not browser 
+
+// readabletty is the best thing in coding 
+
+// code should be readable and futreable 
+
+
+let name = "Dhiraj" // String 
+let age = 18 // number 
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint
+//  String => ""
+// boolean => true/false
+// null => Standalone Value (repragentaion of emty type)
+// undefined => 
+// Symbol => unique
+
+
+
+// object 
+
+
+
+
+
