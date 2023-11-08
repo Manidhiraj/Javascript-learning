142d16d11e1e7c10b50854cb68228130221bdab1
Dhiraj Mani Tiwari <60131419+Manidhiraj@users.noreply.github.com>
2023-11-08 10:57:00 +0000

    How to Declear Variable

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "dhiraj19101@gmail.com"
+var accountPassword = "1234"
+accountCity  = "jaipur"
+let accountStatement;
+
+
+//accountId = 2 not allowed 
+accountEmail = "hello@gmail.com"
+accountPassword = "123456789"
+accountCity = "Banglore"
+
+  
+/* 
+Prefer not to use var 
+becouse of issue in block scope and functional scope 
+
+*/
+
+
+console.log([accountId,accountEmail,accountPassword,accountCity,accountStatement]);
