adf6deeef168c1e4c18bcb1ef01ca9c3ea9eeeaf
Dhiraj Mani Tiwari <60131419+Manidhiraj@users.noreply.github.com>
2023-11-13 05:21:24 +0000

    DataType Summery

---
+++ 01_basic/04_datatypeinterview.js
@@ -0,0 +1,30 @@
+// primitive and non- primitive data type
+// Primitive Data Type (Call by value)
+// 7 types : String, Number, Boolarn, null, undefined, Symbole, BigInt
+
+
+
+const score = 100 
+// we don't define variables 
+// Dymanically typed language
+
+
+// Referance Type (Non - Primitive)
+
+// Array, Objects, Funtions 
+
+// objects are inside {}
+let myObj ={
+    name:"Dhiraj",
+    age: 22
+}
+// ARRAYS ARE DICLARED INSIDE []
+const heros = ["Dhiraj", "Mani", "doga"];
+//function
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof heros)
+
