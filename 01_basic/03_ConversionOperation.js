8f0e71958badd8882d603fa8eb25f71d0f4d0520
Dhiraj Mani Tiwari <60131419+Manidhiraj@users.noreply.github.com>
2023-11-13 04:45:58 +0000

    Operations

--- 01_basic/03_ConversionOperation.js
+++ 01_basic/03_ConversionOperation.js
@@ -16,9 +16,26 @@ let valueInNumber = Number(score)
 let isloggedIn = 1
 
 let booleanIsLoggedIn = (Boolean(isloggedIn));
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 
 // 1 => true; 0 => false
 // "" => false
 // "dhiraj" => true 
+
+
+
+//********************************************* Operations ************************************/
+
+
+let value = 3;
+let negValue = -value
+// console.log(negValue)
+
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
